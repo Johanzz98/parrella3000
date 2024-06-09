8030254a2207891d3707266ab923ed2f66bdef65
Johan <johanzzeroc@gmail.com>
2024-06-09 19:43:55 -0400

    a

--- src/components/product/SectionProducts/ProductosMobile.jsx
+++ src/components/product/SectionProducts/ProductosMobile.jsx
@@ -19,7 +19,7 @@ const cardStyle = {
 };
 
 const cardStyle2 = {
-  width: "140px",
+  width: "150px",
 
   cursor: "pointer",
   fontFamily: "arial",
@@ -62,7 +62,7 @@ const mediaStyle = {
 };
 
 const smallMediaStyle = {
-  height: "160px",
+  height: "180px",
   width: "100%",
   objectFit: "cover",
 };
