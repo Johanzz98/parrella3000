9805bb6f59a91fdbdf8a897d685f9ccb3b470e70
Johan <johanzzeroc@gmail.com>
2024-07-11 22:24:16 -0400

    DLC

--- src/components/Footer/About Us/AboutUs.jsx
+++ src/components/Footer/About Us/AboutUs.jsx
@@ -163,7 +163,7 @@ const AboutUs = () => {
         }}
       >
         <img
-              src={`../../assets/AboutUs/BANNER.PNG`}
+              src={`../../../../public/assets/AboutUs/BANNER.png`}
           alt="logo"
           style={{
             width: '100%',
